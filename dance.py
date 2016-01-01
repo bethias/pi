5ec284f9f5465e299ebb276085190f1632229080
Bethia Seager <bethiaseager@bens-mbp.home>
2016-01-01 22:05:29 +0000

    Modified dance.py

--- dance.py
+++ dance.py
@@ -1,6 +1,8 @@
 import time
 import pi2go
 
+#struct = localtime(start_timer)
+
 pi2go.init()
 
 speed = 40
@@ -8,30 +10,44 @@ fast = 60
 
 dist = pi2go.getDistance()
 
-def dance():
-	for i in range(5):
-		pi2go.forward(fast)
-		time.sleep(1.5)
-		pi2go.spinLeft(speed)
-		time.sleep(1)
-		pi2go.reverse(fast)
-		time.sleep(1.5)
-		pi2go.spinRight(speed)
-		time.sleep(1)
+counter = 0
 
-def lights():
-	for j in range(10):
-		pi2go.setAllLEDs(0, 255, 0)
-		time.sleep(0.5)
-		pi2go.setAllLEDs(255, 0 , 0)
-		time.sleep(0.5)
-		pi2go.setAllLEDs(0, 0, 255)
-		time.sleep(0.5)
+#def dance():
+#	for i in range(5):
+#		pi2go.forward(fast)
+#		time.sleep(1.5)
+#		pi2go.spinLeft(speed)
+#		time.sleep(2)
+#		pi2go.reverse(fast)
+#		time.sleep(1.5)
+#		pi2go.spinRight(speed)
+#		time.sleep(2)
+
+#def lights():
+	#for j in range(10):
+	#	pi2go.setAllLEDs(0, 255, 0)
+	#	time.sleep(0.5)
+	#	pi2go.setAllLEDs(255, 0 , 0)
+	#	time.sleep(0.5)
+	#	pi2go.setAllLEDs(0, 0, 255)
+	#	time.sleep(0.5)
+
+#print ("\nTime Now: " , strftime("%X" , struct))
+
+while dist > 7:
+	pi2go.forward(speed)
+		if False:
+			pi2go.spinLeft(speed)
+			time.sleep(2)
+			continue
+		if counter > 50:
+			pi2go.stop()
+			break
+	counter += 1
 		
-if dist	> 5: 	
-	dance()		
-else: 
-	lights()
-	pi2go.spinLeft(speed)
+#if dist > 5: 	
+#	dance()		
+#else:
+#	pi2go.stop()	
 
 pi2go.cleanup()
